168cd55930de782b662c13501ef179b03bbe6195
IsacJfs <isac.jfs@gmail.com>
2024-02-06 16:31:47 -0300

    :test_tube: adicionando testes

---
+++ backend/bodybuilder/tests/test_viewset.py
@@ -0,0 +1,26 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from bodybuilder.models import Exercise
+from authentication.factories.test_factories import UserFactory
+from rest_framework.authtoken.models import Token
+
+class ExerciseViewSetTest(APITestCase):
+
+    def setUp(self):
+        self.user = UserFactory.create()
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+    def test_create_exercise(self):
+        url = reverse('exercise-list')
+        data = {
+            'name': 'Push Up',
+            'equipment': 'BW',
+            'rep_max': 10.0
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Exercise.objects.count(), 1)
+        self.assertEqual(Exercise.objects.get().name, 'Push Up')
+
