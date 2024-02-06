2e8b675d902d6f9d25f472f9b4e400f58c880f1a
IsacJfs <isac.jfs@gmail.com>
2024-02-06 16:31:15 -0300

    :recycle: [REFACTOR] melhorando performance

--- backend/bodybuilder/models/exercise.py
+++ backend/bodybuilder/models/exercise.py
@@ -1,13 +1,27 @@
 from django.db import models
-from django.conf import settings
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+
+class EquipmentChoices(models.TextChoices):
+    DUMBBELL = 'DB', 'Dumbbell'
+    BARBELL = 'BB', 'Barbell'
+    MACHINE = 'MC', 'Machine'
+    BODYWEIGHT = 'BW', 'Bodyweight'
+    OTHER = 'OT', 'Other'
 
 class Exercise(models.Model):
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    equipment = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=150, unique=True, blank=True)
+    equipment = models.CharField(max_length=100, choices=EquipmentChoices.choices)
     rep_max = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(f"{self.created_by.id}-{self.name}")
+        super().save(*args, **kwargs)
