d0770e73c9a1b9cba5621a4b631cbff7dbe5cb43
Mateus Ceci <mateusceci@gmail.com>
2024-02-29 10:34:56 -0300

    ..

--- backend/crossfit/serializers/workout.py
+++ backend/crossfit/serializers/workout.py
@@ -7,17 +7,27 @@ from crossfit.models.cros_workout import CrosWorkout, WorkoutExercise
 
 
 class WorkoutExerciseSerializer(serializers.ModelSerializer):
+<<<<<<< HEAD
     workout = serializers.PrimaryKeyRelatedField(
         queryset=CrosWorkout.objects.all(), required=False)
     workout = serializers.PrimaryKeyRelatedField(
         queryset=CrosWorkout.objects.all(), required=False)
+=======
+    workout = serializers.PrimaryKeyRelatedField(queryset=CrosWorkout.objects.all(), required=False)
+    workout = serializers.PrimaryKeyRelatedField(queryset=CrosWorkout.objects.all(), required=False)
+    workout = serializers.PrimaryKeyRelatedField(queryset=CrosWorkout.objects.all(), required=False)
+    workout = serializers.PrimaryKeyRelatedField(queryset=CrosWorkout.objects.all(), required=False)
+>>>>>>> e16cbc2 (merging)
     exercise = CrosExerciseSerializer()
 
     class Meta:
         model = WorkoutExercise
         fields = ['workout', 'exercise', 'weight_for_women', 'weight_for_men']
         fields = ['workout', 'exercise', 'weight_for_women', 'weight_for_men']
+<<<<<<< HEAD
 
+=======
+>>>>>>> e16cbc2 (merging)
 
 class WorkoutSerializer(serializers.ModelSerializer):
     exercises = WorkoutExerciseSerializer(
@@ -33,9 +43,11 @@ class WorkoutSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         exercises_data = validated_data.pop('workoutexercise_set', [])
         exercises_data = validated_data.pop('workoutexercise_set', [])
+        exercises_data = validated_data.pop('workoutexercise_set', [])
         workout = CrosWorkout.objects.create(**validated_data)
 
         for exercise_data in exercises_data:
+<<<<<<< HEAD
             exercise_serializer = CrosExerciseSerializer(
                 data=exercise_data['exercise'])
             if exercise_serializer.is_valid():
@@ -51,9 +63,16 @@ class WorkoutSerializer(serializers.ModelSerializer):
             else:
                 raise serializers.ValidationError(exercise_serializer.errors)
 
+=======
+            exercise_data['workout'] = workout
+            WorkoutExercise.objects.create(**exercise_data)
+            exercise_data['workout'] = workout
+            WorkoutExercise.objects.create(**exercise_data)
+>>>>>>> e16cbc2 (merging)
         return workout
 
     def update(self, instance, validated_data):
+        exercises_data = validated_data.pop('workoutexercise_set', [])
         exercises_data = validated_data.pop('workoutexercise_set', [])
         exercises_data = validated_data.pop('workoutexercise_set', [])
         instance.name = validated_data.get('name', instance.name)
@@ -63,12 +82,18 @@ class WorkoutSerializer(serializers.ModelSerializer):
         instance.time_cap = validated_data.get('time_cap', instance.time_cap)
         instance.save()
 
+<<<<<<< HEAD
         existing_exercises_ids = [
             exercise.exercise.id for exercise in instance.workoutexercise_set.all()]
         existing_exercises_ids = [
             exercise.exercise.id for exercise in instance.workoutexercise_set.all()]
         existing_exercises_ids = [
             exercise.exercise.id for exercise in instance.workoutexercise_set.all()]
+=======
+        existing_exercises_ids = [exercise.exercise.id for exercise in instance.workoutexercise_set.all()]
+        existing_exercises_ids = [exercise.exercise.id for exercise in instance.workoutexercise_set.all()]
+        existing_exercises_ids = [exercise.exercise.id for exercise in instance.workoutexercise_set.all()]
+>>>>>>> e16cbc2 (merging)
         for exercise_data in exercises_data:
             exercise_id = exercise_data.get('exercise').id
             if exercise_id in existing_exercises_ids:
