72a42a52f33f84efe789085a5c824d9ef3f8cf10
Mateus Ceci <mateusceci@gmail.com>
2024-03-19 11:39:08 -0300

    change upload image

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birthday = models.DateField(null=True, blank=True)
     height = models.FloatField(null=True, blank=True)
     weight = models.FloatField(null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_picture = models.ImageField(upload_to=user_directory_path, null=True, blank=True)
     gender = models.CharField(max_length=6, null=True, blank=True)
 
     def __str__(self):
