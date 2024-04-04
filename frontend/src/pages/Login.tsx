77018827f9dbe34ba0923292d06b040d40d99c5d
Mateus Ceci <mateusceci@gmail.com>
2024-04-04 15:10:48 -0300

    feat: implementing loading in sign in and login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -6,8 +6,10 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Loading from "./Loading";
 
 export default function Login() {
+  const [loading, setLoading] = useState(false);
   const [userData, setUserData] = useState({
     username: "",
     password: "",
@@ -26,6 +28,7 @@ export default function Login() {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setLoading(true);
     const username = userData.username;
     const password = userData.password;
 
@@ -44,46 +47,54 @@ export default function Login() {
     } catch (err) {
       console.log(err);
       toast.error("Login failed. Please try again.");
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <section className="h-screen bg-grayBg flex flex-col gap-10 justify-center items-center">
-      <h1 className="head-text">Welcome to the App!</h1>
-      <form
-        onSubmit={handleSubmit}
-        className="p-12 rounded-2xl w-96 bg-white shadow-2xl"
-      >
-        <h2 className="text-2xl text-center mb-10">LOGIN</h2>
-        <Label htmlFor="username">Username:</Label>
-        <Input
-          data-test="username-login"
-          className="mb-8"
-          name="username"
-          type="text"
-          onChange={handleInputChange}
-          autoComplete="username"
-          required
-        />
-        <Label htmlFor="password">Password:</Label>
-        <Input
-          data-test="password-login"
-          className="mb-10"
-          type="password"
-          name="password"
-          onChange={handleInputChange}
-          required
-        />
-        <Button className="w-full mb-2" type="submit">
-          Login
-        </Button>
-        <p className="text-sm text-center">
-          Don't have an account?{" "}
-          <Link to="/signin" className="text-mainBlue">
-            Create now!
-          </Link>{" "}
-        </p>
-      </form>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <h1 className="head-text">Welcome to the App!</h1>
+          <form
+            onSubmit={handleSubmit}
+            className="p-12 rounded-2xl w-96 bg-white shadow-2xl"
+          >
+            <h2 className="text-2xl text-center mb-10">LOGIN</h2>
+            <Label htmlFor="username">Username:</Label>
+            <Input
+              data-test="username-login"
+              className="mb-8"
+              name="username"
+              type="text"
+              onChange={handleInputChange}
+              autoComplete="username"
+              required
+            />
+            <Label htmlFor="password">Password:</Label>
+            <Input
+              data-test="password-login"
+              className="mb-10"
+              type="password"
+              name="password"
+              onChange={handleInputChange}
+              required
+            />
+            <Button className="w-full mb-2" type="submit">
+              Login
+            </Button>
+            <p className="text-sm text-center">
+              Don't have an account?{" "}
+              <Link to="/signin" className="text-mainBlue">
+                Create now!
+              </Link>{" "}
+            </p>
+          </form>
+        </>
+      )}
     </section>
   );
 }
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -5,9 +5,11 @@ import { ChangeEvent, useState } from "react";
 import { useRegister } from "@/api/register/useRegister";
 import { RegisterData } from "@/api/register/types";
 import { useNavigate } from "react-router-dom";
+import Loading from "./Loading";
 
 export default function Signin() {
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
   const [userData, setUserData] = useState({
     first_name: "",
     last_name: "",
@@ -32,6 +34,7 @@ export default function Signin() {
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setLoading(true);
 
     if (userData.password !== confirmPassword) {
       setSamePassword(false);
@@ -47,72 +50,82 @@ export default function Signin() {
       navigate("/login");
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <section className="bg-grayBg flex flex-col gap-10 justify-center items-center">
-      <h1 className="head-text mt-8">Welcome to the App!</h1>
-      <form
-        onSubmit={handleSubmit}
-        className="p-8 rounded-2xl w-96 bg-white shadow-2xl mb-16"
-      >
-        <h2 className="text-2xl text-center mb-10">Sign in</h2>
-        <Label htmlFor="username">Username:</Label>
-        <Input
-          className="mb-8"
-          name="username"
-          type="text"
-          onChange={handleInputChange}
-          required
-        />
-        <Label htmlFor="email">Email:</Label>
-        <Input
-          className="mb-8"
-          name="email"
-          type="email"
-          onChange={handleInputChange}
-          required
-        />
-        <Label htmlFor="username">First Name:</Label>
-        <Input
-          className="mb-8"
-          name="first_name"
-          type="text"
-          onChange={handleInputChange}
-          required
-        />
-        <Label htmlFor="lastname">Last Name:</Label>
-        <Input
-          className="mb-8"
-          name="last_name"
-          type="text"
-          onChange={handleInputChange}
-          required
-        />
-        <Label htmlFor="password">Password:</Label>
-        <Input
-          className="mb-10"
-          type="password"
-          name="password"
-          onChange={handleInputChange}
-          required
-        />
-        <Label htmlFor="confirmPassword">Confirm Password:</Label>
-        <Input
-          className="mb-10"
-          type="password"
-          name="confirmPassword"
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-        {!samePassword && (
-          <p className="text-sm text-red-500 pb-1">Passwords does not match</p>
-        )}
-        <Button className="w-full" type="submit">
-          Sign in
-        </Button>
-      </form>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <h1 className="head-text mt-8">Welcome to the App!</h1>
+          <form
+            onSubmit={handleSubmit}
+            className="p-8 rounded-2xl w-96 bg-white shadow-2xl mb-16"
+          >
+            <h2 className="text-2xl text-center mb-10">Sign in</h2>
+            <Label htmlFor="username">Username:</Label>
+            <Input
+              className="mb-8"
+              name="username"
+              type="text"
+              onChange={handleInputChange}
+              required
+            />
+            <Label htmlFor="email">Email:</Label>
+            <Input
+              className="mb-8"
+              name="email"
+              type="email"
+              onChange={handleInputChange}
+              required
+            />
+            <Label htmlFor="username">First Name:</Label>
+            <Input
+              className="mb-8"
+              name="first_name"
+              type="text"
+              onChange={handleInputChange}
+              required
+            />
+            <Label htmlFor="lastname">Last Name:</Label>
+            <Input
+              className="mb-8"
+              name="last_name"
+              type="text"
+              onChange={handleInputChange}
+              required
+            />
+            <Label htmlFor="password">Password:</Label>
+            <Input
+              className="mb-10"
+              type="password"
+              name="password"
+              onChange={handleInputChange}
+              required
+            />
+            <Label htmlFor="confirmPassword">Confirm Password:</Label>
+            <Input
+              className="mb-10"
+              type="password"
+              name="confirmPassword"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              required
+            />
+            {!samePassword && (
+              <p className="text-sm text-red-500 pb-1">
+                Passwords does not match
+              </p>
+            )}
+            <Button className="w-full" type="submit">
+              Sign in
+            </Button>
+          </form>
+        </>
+      )}
     </section>
   );
 }
